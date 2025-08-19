637cca00047b5268ed9c3c2724fe45cccee6042f
Fahad Ullah Khan <fahadullahk4@gmail.com>
2025-08-19 19:15:31 +0500

    Flavor slider section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+	useGSAP(() => {
+		ScrollSmoother.create({
+			smooth: 3,
+			effects: true,
+		});
+	});
 	return (
 		<main>
 			<Navbar />
-			<HeroSection />
-      <MessageSection />
-      <div className="h-dvh"></div>
+			<div id="smooth-wrapper">
+				<div id="smooth-content">
+					<HeroSection />
+					<MessageSection />
+					<FlavorSection />
+					<div className="h-dvh"></div>
+				</div>
+			</div>
 		</main>
 	);
 };
